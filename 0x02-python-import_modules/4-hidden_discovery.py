e52b3f4c82c8b6232bc7f06eea2c04328629d813
IYKE <106726596+iyke-e@users.noreply.github.com>
2022-12-02 04:44:15 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
 import sys
+import hidden_4 as hidden
 
 if __name__ != "__main__":
     exit()
 
-argStr = "{:d} argument"
-argc = len(sys.argv) - 1
-if argc == 0:
-    argStr += 's.'
-elif argc == 1:
-    argStr += ':'
-else:
-    argStr += 's:'
-print(argStr.format(argc))
-
-i = 0
-for arg in sys.argv:
-    if i != 0:
-        print("{:d}: {:s}".format(i, arg))
-    i += 1
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
